3ab65405ff82a1c363e8672eaa982b373acb264e
Dragos-Daniel Bonaparte <dragosbonaparte@gmail.com>
2024-03-06 23:10:38 +0100

    UpdateEventDescriptionAggregateTests.cs DONE

--- VIAEventAssociation/Tests/UnitTests/Features/Event/UpdateDescription/UpdateEventDescriptionAggregateTests.cs
+++ VIAEventAssociation/Tests/UnitTests/Features/Event/UpdateDescription/UpdateEventDescriptionAggregateTests.cs
@@ -1,66 +1,86 @@
+using VIAEventAssociation.Core.Tools.OperationResult.Result;
+using Xunit.Abstractions;
+
+namespace UnitTests.Features.Event.UpdateDescription;
+
+using Domain.Aggregates.Events;
 using Domain.Aggregates.Guests;
 using Domain.Common.Enums;
 using Xunit;
 
-namespace UnitTests.Features.Event.UpdateDescription;
-using Domain.Aggregates.Events;
 public class UpdateEventDescriptionAggregateTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
     private Event _event;
-    
-    public UpdateEventDescriptionAggregateTests()
+
+    public UpdateEventDescriptionAggregateTests(ITestOutputHelper testOutputHelper)
     {
-        _event = new Event(0, "Title", "Description", DateTime.Now, DateTime.Now, 30, EventVisibility.Public, EventStatus.Active, new List<Domain.Aggregates.Guests.Guest>());
+        _testOutputHelper = testOutputHelper;
+        _event = new Event(0, "Title", "Description", DateTime.Now, DateTime.Now, 30, EventVisibility.Public,
+            EventStatus.Active, new List<Domain.Aggregates.Guests.Guest>());
     }
-    
+
     [Fact]
     public void GivenGoodDescription_WhenUpdatingDescription_ThenDescriptionIsUpdated()
     {
         // Arrange
         var description = "New Description";
-        
+
         // Act
-        _event.UpdateDescription(description);
-        
+        var result = _event.UpdateDescription(description);
+
+        if (result is ResultFailure<Event>)
+            foreach (var error in result.GetMessages()!)
+                _testOutputHelper.WriteLine(error.GetMessage());
+
         // Assert
         Assert.Equal(description, _event.GetDescription());
     }
-    
+
     [Fact]
     public void GivenNoDescription_WhenUpdatingDescription_ThenDescriptionIsNotUpdated()
     {
         // Arrange
         var description = "";
-        
+
         // Act
-        _event.UpdateDescription(description);
-        
+        var result = _event.UpdateDescription(description);
+        if (result is ResultFailure<Event>)
+            foreach (var error in result.GetMessages()!)
+                _testOutputHelper.WriteLine(error.GetMessage());
+
         // Assert
         Assert.NotEqual(description, _event.GetDescription());
-    } 
-    
+    }
+
     [Fact]
     public void GivenTooLongDescription_WhenUpdatingDescription_ThenDescriptionIsNotUpdated()
     {
         // Arrange
-        string description = new string('a', 1001);
-        
+        string description = new string('a', 701);
+
         // Act
-        _event.UpdateDescription(description);
-        
+        var result = _event.UpdateDescription(description);
+        if (result is ResultFailure<Event>)
+            foreach (var error in result.GetMessages()!)
+                _testOutputHelper.WriteLine(error.GetMessage());
+
         // Assert
         Assert.NotEqual(description, _event.GetDescription());
     }
-    
+
     [Fact]
     public void GivenTooShortDescription_WhenUpdatingDescription_ThenDescriptionIsNotUpdated()
     {
         // Arrange
         string description = new string('a', 10);
-        
+
         // Act
-        _event.UpdateDescription(description);
-        
+        var result = _event.UpdateDescription(description);
+        if (result is ResultFailure<Event>)
+            foreach (var error in result.GetMessages()!)
+                _testOutputHelper.WriteLine(error.GetMessage());
+
         // Assert
         Assert.NotEqual(description, _event.GetDescription());
     }
