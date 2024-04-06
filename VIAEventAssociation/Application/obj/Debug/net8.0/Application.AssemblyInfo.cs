e67fbba28b6055daf3a633a917a5c877237f9c9d
YoUnGi102 <90184551+YoUnGi102@users.noreply.github.com>
2024-04-06 15:32:46 +0200

    Add Use Case 19 - Handler, command and tests

--- VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd12b6e6af5d439d84c6564f4e9ed14e228309a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fe81b6923b5bba9ed952ee63ddeed9777f09d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ VIAEventAssociation/Tests/UnitTests/Features/Event/SetLocation/UpdateEventLocationCommandTests.cs
@@ -0,0 +1,36 @@
+﻿using ViaEventAssociation.Core.Application.Features;
+using VIAEventAssociation.Core.Tools.OperationResult.Result;
+using Xunit;
+
+namespace VIAEventAssociation.Tests.UnitTests.Features.Event.Activate;
+
+public class SetEventLocationCommandTests
+{
+
+    [Fact]
+    public async Task GivenValidData_WhenSettingLocation_ThenLocationSet()
+    {
+        // Arrange
+        Result<SetEventLocationCommand> result = SetEventLocationCommand.Create(1, 1);
+        SetEventLocationCommand command = result.GetObj();
+        
+        // Assert
+        Assert.False(result.IsFailure());
+        Assert.NotNull(command);
+        Assert.NotNull(command.EventId.ToString());
+        Assert.NotNull(command.LocationId.ToString());
+    }
+    
+    [Fact]
+    public async Task GivenInvalidData_WhenSettingLocation_ThenLocationNotSet()
+    {
+        // Arrange
+        Result<SetEventLocationCommand> result = SetEventLocationCommand.Create(1, -1);
+        SetEventLocationCommand command = result.GetObj();
+        
+        // Assert
+        Assert.True(result.IsFailure());
+        Assert.Null(command);
+    }
+    
+}
