63cfaa9e7aaa22b9f7ca8fc0610b2e4ed0d3f290
YoUnGi102 <90184551+YoUnGi102@users.noreply.github.com>
2024-04-06 10:47:46 +0200

    Add Update Start and End Date Time handler, command and tests

--- VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e909d4bd8d7e8927e2cf6eaa91ddf0a50a294c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105dd45c1b865dc1aed431bf2dfe72a5a2828f8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
