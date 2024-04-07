72815db9b050692ccb7ae377012ea0e40223d67e
Dragoselul <315261@via.dk>
2024-04-07 17:36:43 +0200

    Added CommandDispatcher.cs

--- VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6e6ddf20307156d2e8b891b8f27e8bfbe349e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4744d3765e25bf891f1af3d7675663b001e4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
