1e306d46dd2843f8838c240da134abd4505976f1
Dragoselul <315261@via.dk>
2024-04-11 18:33:27 +0200

    Update .gitignore

--- VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ VIAEventAssociation/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15520a9cef98518952b573cc6f2e58e8ec36b76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb9af1faa62cbd2314308412db940f99bdb8169")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
