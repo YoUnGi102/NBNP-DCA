a99beae4c87ee02c5974d083fe25208110fc997e
Dragoselul <315261@via.dk>
2024-04-29 19:55:15 +0200

    IMapper and ObjectMapper

--- VIAEventAssociation/src/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ VIAEventAssociation/src/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273a3ae2994952d8e4d568cefe3d01517eed22ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2289075d90c32ac7f5730da70e394ea9ae9810b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
