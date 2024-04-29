b791515bbffdbacb2b8d37764816ed76f1473501
Dragoselul <315261@via.dk>
2024-04-29 21:55:40 +0200

    SendInvitation.cs made for the creator

---
+++ VIAEventAssociation/src/WebAPI/Endpoints/Creator/SendInvitation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViaEventAssociation.Core.Application.AppEntry.CommandDispatching.Commands.Creator;
+using ViaEventAssociation.Core.Application.AppEntry.CommandDispatching.Dispatcher;
+using VIAEventAssociation.Core.Tools.OperationResult.Result;
+using WebAPI.Endpoints.Common;
+
+namespace WebAPI.Endpoints.Creator;
+
+
+public record SendInvitationResponse(bool Success);
+
+public record SendInvitationRequest([FromBody] int GuestID, [FromBody] int EventId);
+
+public class SendInvitation(ICommandDispatcher dispatcher): ApiEndpoint.WithRequest<SendInvitationRequest>.WithResponse<SendInvitationResponse>
+{
+    [HttpPost("creator/invitation/send")]
+    public override async Task<ActionResult<SendInvitationResponse>> HandleAsync([FromBody] SendInvitationRequest request)
+    {
+        Result<SendInvitationCommand> commandResult = SendInvitationCommand.Create(request.GuestID, request.EventId);
+        if (commandResult is ResultFailure<SendInvitationCommand>)
+            return Ok(new SendInvitationResponse(false));
+
+        Result<None> result = await dispatcher.DispatchAsync(commandResult.GetObj());
+
+        if (result is ResultSuccess<None>)
+        {
+            return Ok(new SendInvitationResponse(true));
+        }
+
+        return BadRequest(new SendInvitationResponse(false));
+    }
+}
