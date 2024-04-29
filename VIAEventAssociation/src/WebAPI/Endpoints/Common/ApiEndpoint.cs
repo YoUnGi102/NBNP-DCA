4fa38d1c51492de0bfcda1a4fef07c28237e827b
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-04-29 19:57:00 +0200

    Api endpoint

---
+++ VIAEventAssociation/src/WebAPI/Endpoints/Common/ApiEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Endpoints.Common;
+
+public class ApiEndpoint
+{
+    public static class WithRequest<TRequest>
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync(TRequest request);
+        }
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync(TRequest request);
+        }
+    }
+    public static class WithoutRequest
+    {
+        public abstract class WithResponse<TResponse> : EndpointBase
+        {
+            public abstract Task<ActionResult<TResponse>> HandleAsync();
+        }
+        public abstract class WithoutResponse : EndpointBase
+        {
+            public abstract Task<ActionResult> HandleAsync();
+        }
+    }
+}
+[ApiController, Route("api")]
+public abstract class EndpointBase : ControllerBase;
