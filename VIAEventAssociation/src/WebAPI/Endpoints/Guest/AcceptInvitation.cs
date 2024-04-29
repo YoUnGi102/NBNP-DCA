742d2bd22dc787aa3c7530313cbf513ed9558cd1
Dragoselul <315261@via.dk>
2024-04-29 21:07:16 +0200

    Fixed accept invitation endpoint

--- VIAEventAssociation/src/WebAPI/Endpoints/Guest/AcceptInvitation.cs
+++ VIAEventAssociation/src/WebAPI/Endpoints/Guest/AcceptInvitation.cs
@@ -1,30 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ObjectMapper;
 using ViaEventAssociation.Core.Application.AppEntry.CommandDispatching.Commands.Guest;
 using ViaEventAssociation.Core.Application.AppEntry.CommandDispatching.Dispatcher;
+using VIAEventAssociation.Core.Tools.OperationResult.Result;
 using WebAPI.Endpoints.Common;
 
 namespace WebAPI.Endpoints.Guest;
 
-public record AcceptInvitationResponse(bool success);
+public record AcceptInvitationResponse(bool Success);
 public record AcceptInvitationRequest([FromBody] string Email, [FromBody] int EventId);
 
-public class AcceptInvitation : ApiEndpoint.WithRequest<AcceptInvitationRequest>.WithResponse<AcceptInvitationResponse>
+public class AcceptInvitation(ICommandDispatcher dispatcher) : ApiEndpoint.WithRequest<AcceptInvitationRequest>.WithResponse<AcceptInvitationResponse>
 {
-    private readonly ICommandDispatcher _dispatcher;
 
-    public AcceptInvitation(ICommandDispatcher dispatcher)
-    {
-        _dispatcher = dispatcher;
-    }
-
-    [HttpPost("invitation")]
+    [HttpPost("guests/invitation")]
     public override async Task<ActionResult<AcceptInvitationResponse>> HandleAsync([FromBody] AcceptInvitationRequest request)
     {
-        AcceptInvitationCommand command = AcceptInvitationCommand.Create(request.Email, request.EventId).GetObj()!;
-
-        // Assuming you dispatch the command here and handle the result accordingly
-        var result = await _dispatcher.DispatchAsync(command);
+        Result<AcceptInvitationCommand> commandResult = AcceptInvitationCommand.Create(request.Email, request.EventId);
+        if(commandResult is ResultFailure<AcceptInvitationCommand>)
+            return Ok(new AcceptInvitationResponse(false));
+        
+        var result = await dispatcher.DispatchAsync(commandResult.GetObj());
 
         if (!result.IsFailure())
         {
@@ -32,7 +27,7 @@ public class AcceptInvitation : ApiEndpoint.WithRequest<AcceptInvitationRequest>
         }
         else
         {
-            return Ok(new AcceptInvitationResponse(false)); // Or more detailed error handling as necessary
+            return Ok(new AcceptInvitationResponse(false));
         }
     }
 }
